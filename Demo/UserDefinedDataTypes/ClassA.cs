41c1873f4a7d49d6bd3d3c7c65ee8c09ea98ab54
Ahmed-Hany2 <ahmedhanypro2@gmail.com>
2025-07-19 23:09:12 +0300

    video 2 (user-defined data types)

---
+++ Demo/UserDefinedDataTypes/ClassA.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Demo.UserDefinedDataTypes
+{
+    internal class ClassA : ITypeA
+    {
+        int X;
+        public ClassA(int x)
+        {
+            X = x;
+        }
+        public int Y { get; set; }
+
+        void ITypeA.Print()
+        {
+            Console.WriteLine("Print ClassA");
+        }
+    }
+}
---
+++ Demo/UserDefinedDataTypes/ITypeA.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.UserDefinedDataTypes
+{
+    internal interface ITypeA
+    {
+        void Print();
+        public int Y { get; set; }
+
+        void PrintHello()
+        {
+            Console.WriteLine("SayHello");
+        }
+}

b4040d27439396c8c76fb21fa7a656e936e01947
Ahmed-Hany2 <ahmedhanypro2@gmail.com>
2025-07-19 23:21:00 +0300

    video 4 (Enum Example 01)

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace Demo
+﻿using Demo.UserDefinedDataTypes;
+using Demo.UserDefinedDataTypes.Enums;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            #region video 4 (Enum Example 01)
+            Person person = new Person();
+            person.Id = 1;
+            person.Name = "Ahmed";
+            person.gender = Gender.Male;
+
+            Console.WriteLine(person.gender);
+
+            Grade grade = Grade.A;
+            if (grade == Grade.A)
+            {
+                Console.WriteLine("Excellent");
+            }
+            else if (grade == Grade.B)
+            {
+                Console.WriteLine("Good");
+            }
+            else if (grade == Grade.C)
+            {
+                Console.WriteLine("Average");
+            }
+            else
+            {
+                Console.WriteLine("Poor");
+            }
+
+            grade = (Grade)5;
+            Console.WriteLine(grade);
+            #endregion
+
+
         }
     }
 }

fc91e8516493ef3740fe8fae8be8b54bf900ec0c
Ahmed-Hany2 <ahmedhanypro2@gmail.com>
2025-07-19 22:30:26 +0300

    Question 1

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Assignment
+﻿using Assignment.Enums;
+using System.ComponentModel;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //1 - Create an enum called "WeekDays" with the days of the week
+            //(Monday to Sunday) as its members.Then, write a C# program that
+            //    prints out all the days of the week using this enum.
+
+            string[] weekDays = Enum.GetNames(typeof(WeekDays));
+            Console.WriteLine("Days of the week:");
+            foreach (string day in weekDays)
+            {
+                Console.WriteLine(day);
+            }
+
         }
     }
 }
